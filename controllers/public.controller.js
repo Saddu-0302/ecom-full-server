617eae0110a3fd173de3f68a9e2aaf06640083b8
Your Name <you@example.com>
2024-06-22 14:31:57 +0530

    public and user route added

--- controllers/public.controller.js
+++ controllers/public.controller.js
@@ -3,9 +3,9 @@ const Product = require("../models/Product")
 
 exports.publicGetAllProducts = asyncHandler(async (req, res) => {
     const result = await Product.find({ active: true })
-    res.json({ message: "Product fetch success" })
+    res.json({ message: "Product fetch success", result })
 })
 exports.publicGetProductDetails = asyncHandler(async (req, res) => {
     const result = await Product.findById(req.params.id)
-    res.json({ message: "Product fetch success" })
+    res.json({ message: "Product fetch success", result })
 })
