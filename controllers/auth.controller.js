85f7f741c39c6744436b1de0259931a445e4fad2
Your Name <you@example.com>
2024-06-26 14:52:36 +0530

    route update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -45,9 +45,7 @@ exports.registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body
     const hash = await bcrypt.hash(password, 10)
     await User.create({ name, email, password: hash })
-    if (found) {
-        return res.status(401).json({ message: "Email already Registered" })
-    }
+
     res.json({ message: "User Register Success" })
 })
 exports.loginUser = asyncHandler(async (req, res) => {
